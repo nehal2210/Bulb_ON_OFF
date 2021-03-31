4d4460afe3443c095b5ef4fdd69f6b488fe5b518
Nehal Ahmed <41865951+nehal2210@users.noreply.github.com>
2021-03-31 14:41:13 -0700

    version 1

---
+++ BULB_ON_OFF/app.js
@@ -0,0 +1,11 @@
+function on(){
+    var b = document.getElementById("b")
+    b.src="images/on.jpg";    
+    document.body.style.backgroundColor='white';
+}
+
+function off(){
+    var b = document.getElementById("b")
+    b.src="images/off.jpg";    
+    document.body.style.backgroundColor = "black";
+}
